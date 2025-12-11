ae7364bcbcecca2b3d71a0511a2a6ed22939f380
LeAnn <eyesseekreality0@gmail.com>
2025-12-11 16:08:31 -0700

    UI: remove hero scroll indicator for cleaner header

--- components/home/hero-section.tsx
+++ components/home/hero-section.tsx
@@ -31,12 +31,7 @@ export function HeroSection() {
         <GoldDivider className="mt-16" />
       </div>
 
-      {/* Scroll Indicator */}
-      <div className="absolute bottom-8 left-1/2 -translate-x-1/2 animate-bounce">
-        <div className="w-6 h-10 border-2 border-gold/50 rounded-full flex justify-center pt-2">
-          <div className="w-1 h-3 bg-gold rounded-full" />
-        </div>
-      </div>
+      {/* Scroll indicator removed for a cleaner hero */}
     </section>
   )
 }
